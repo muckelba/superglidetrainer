1806011a4d2382b0353262061cc319a784b6b648
muckelba <34460584+muckelba@users.noreply.github.com>
2023-03-06 15:31:58 +0100

    move to components

---
+++ src/lib/components/Faq.svelte
@@ -0,0 +1,84 @@
+<script>
+    let faqHidden = true;
+
+    function toggleFaq() {
+        faqHidden = !faqHidden;
+    }
+</script>
+
+<div class="card">
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <a class="card-header" on:click={toggleFaq}>
+        <button class="card-header-icon" aria-label="collapse the FAQ">
+            <i class="fa fa-angle-{faqHidden ? 'down' : 'up'}" />
+        </button>
+        <p class="card-header-title title is-4">FAQ</p>
+    </a>
+    <div class="card-content {faqHidden ? 'is-hidden' : ''}">
+        <div class="content">
+            <h5 class="title is-5">
+                What is a Superglide and why should i care about learning it?
+            </h5>
+            <p class="subtitle is-6">
+                A Superglide is an instant 1 Frame acceleration out of a Mantle.
+                Beyond just being fun this is a great way to throw of your
+                enemies aim, escape and/or chase someone or even get across gaps
+                that are not possible to gap without the usage of movement
+                abilities.
+            </p>
+        </div>
+        <div class="content">
+            <h5 class="title is-5">
+                Why is there a Chance? I thought it just needed Frame perfect
+                inputs?
+            </h5>
+            <p class="subtitle is-6">
+                Because of the way Apex handles Input data, you might have timed
+                your jump and crouch input very close to 1 FPS apart. But they
+                might still be processed by the engine on the same Frame. Or 2
+                Frames apart. When it only works when they are processed 1 Frame
+                apart. That is also the reason why the feedback can say to
+                crouch later or sooner. Because of that randomness you can never
+                reach 100% Superglide consistency. Max is 99%. Here is a
+                fantastic video explaining the exact same Problem in Titanfall
+                2:
+            </p>
+            <p>
+                <iframe
+                    class="has-ratio"
+                    width="560"
+                    height="315"
+                    src="https://www.youtube-nocookie.com/embed/Cwa0qbDx2dA?start=423"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen
+                />
+            </p>
+        </div>
+        <div class="content">
+            <h5 class="title is-5">What do the Analytics mean?</h5>
+            <p class="subtitle is-6">blablabla replace me</p>
+        </div>
+        <div class="content">
+            <h5 class="title is-5">Who made this Trainer?</h5>
+            <p class="subtitle is-6">
+                This website is made by <a href="https://github.com/muckelba"
+                    >@muckelba</a
+                >
+                and based on a
+                <a
+                    href="https://github.com/AngryGroceries/Apex_Superglide_Practice_Tool"
+                    >powershell script</a
+                >
+                that
+                <a href="https://github.com/AngryGroceries">@AngryGroceries</a>
+                and
+                <a href="https://github.com/JayTheYggdrasil">@JayTheYggdrasil</a
+                >
+                made.
+            </p>
+        </div>
+    </div>
+</div>
---
+++ src/lib/components/Tips.svelte
@@ -0,0 +1,76 @@
+<script>
+    import Faq from "./Faq.svelte";
+
+    let tipsHidden = true;
+    function toggleHidden() {
+        tipsHidden = !tipsHidden;
+    }
+</script>
+
+<div class="card">
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <a class="card-header" on:click={toggleHidden}>
+        <button class="card-header-icon" aria-label="collapse helpful tips">
+            <i class="fa fa-angle-{tipsHidden ? 'down' : 'up'}" />
+        </button>
+        <p class="card-header-title title is-4">Helpful Tips to get better</p>
+    </a>
+    <div class="card-content {tipsHidden ? 'is-hidden' : ''}">
+        <div class="content">
+            <h5 class="title is-5">
+                Get better at hitting the Jump -> Crouch Timing
+            </h5>
+            <strong>Mouse and Keyboard:</strong>
+            <ol type="1">
+                <li>
+                    It's common to put your crouch on a button next to your jump
+                    input, so you can press them both with 1 finger at the same
+                    time. <code>C</code>,<code>V</code>
+                    or <code>B</code> with spacebar for example.
+                </li>
+                <li>
+                    Different keycap profiles will naturally alter which button
+                    you hit first. So flipping over your crouch keycap can help.
+                    As well as getting different keycaps. Or taping stuff to
+                    your existing keycaps.
+                </li>
+                <li>
+                    Some folks have seen success by jumping with Scrollwheel.
+                </li>
+                <li>
+                    Your keyboard switches will also alter when a key is
+                    activated. Tactile switches like MX blues make it much
+                    easier to feel the activation point, and putting different
+                    switches on your <code>C</code>,<code>V</code>
+                    and <code>B</code> then on Spacebar might help with activation
+                    as well.
+                </li>
+                <li>
+                    The Ultimate Hardware Change would be to get a new Keyboard.
+                    Either to get away from a low polling rate, which introduces
+                    further randomness. Or to get a Keyboard with activation
+                    points you can change. Like from Wooting or the Apex Mini
+                    Pro from Steelseries
+                </li>
+            </ol>
+            <strong>Controller:</strong> <br />
+            For controller the eastiest way is to press either A+B with one finger
+            or X+Square with one finger.
+        </div>
+        <div class="content">
+            <h5 class="title is-5">
+                Get better at timing the Superglide during the Mantle
+            </h5>
+            <div class="subtitle is-6">
+                <ul>
+                    <li>Audio cue</li>
+                    <li>Camera Shake</li>
+                </ul>
+                If you don't gain any Speed you hit the Superglide to early. If you
+                jump of the ledge with speed but not a Superglide you hit it too
+                late.
+            </div>
+        </div>
+    </div>
+</div>
