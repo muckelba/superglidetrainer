652645a9f854b41f7f69bfff89eee109f0ee338e
muckelba <34460584+muckelba@users.noreply.github.com>
2023-03-07 00:04:48 +0100

    re-ordering

--- src/lib/components/Faq.svelte
+++ src/lib/components/Faq.svelte
@@ -16,19 +16,7 @@
         <p class="card-header-title title is-3">FAQ</p>
     </a>
     <div class="card-content {faqHidden ? 'is-hidden' : ''}">
-        <h4 class="title is-4 is-underlined">General Superglide Questions:</h4>
-        <div class="content">
-            <h5 class="title is-5">
-                What is a Superglide and why should i care about learning it?
-            </h5>
-            <p class="subtitle is-6">
-                A Superglide is an instant 1 frame acceleration out of a mantle.
-                Beyond just being fun this is a great way to throw of your
-                enemies aim, escape and/or chase someone or even get across gaps
-                that are not possible to gap without the usage of movement
-                abilities.
-            </p>
-        </div>
+        <h4 class="title is-4 is-underlined">Superglide Trainer Questions:</h4>
         <div class="content">
             <h5 class="title is-5">
                 Why is there a Chance? I thought it just needed Frame perfect
@@ -74,10 +62,28 @@
                 install could be the cause.
             </p>
         </div>
-        <h4 class="title is-4 is-underlined">Superglide Trainer Questions:</h4>
         <div class="content">
             <h5 class="title is-5">What do the Analytics mean?</h5>
             <p class="subtitle is-6">blablabla replace me</p>
+        </div>
+        <h4 class="title is-4 is-underlined">General Superglide Questions:</h4>
+        <div class="content">
+            <h5 class="title is-5">
+                What is a Superglide and why should i care about learning it?
+            </h5>
+            <p class="subtitle is-6">
+                A Superglide is an instant 1 frame acceleration out of a mantle.
+                Beyond just being fun this is a great way to throw of your
+                enemies aim, escape and/or chase someone or even get across gaps
+                that are not possible to gap without the usage of movement
+                abilities.
+            </p>
+        </div>
+        <div class="content">
+            <h5 class="title is-5">
+                How do i know if i hit a Superglide in game?
+            </h5>
+            <p class="subtitle is-6">foo</p>
         </div>
     </div>
 </div>
