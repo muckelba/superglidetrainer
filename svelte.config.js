de642d99c33d5550c80c3c25bc30baa86fe1f1a2
muckelba <34460584+muckelba@users.noreply.github.com>
2023-03-23 02:39:25 +0100

    idk

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const dev = process.argv.includes("dev");
 const config = {
   kit: {
     adapter: adapter(),
-    paths: { base: '/superglidetrainer' }
+    paths: { base: dev ? '' : '/superglidetrainer' }
   },
 
   preprocess: [
